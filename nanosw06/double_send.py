49b8100eee995d5bec383714b0b6a7b10dfd8338
Y <yasuda@Lily-2.local>
2026-04-17 14:40:04 +0900

    added b for the type symbol of bytes string

--- nanosw06/double_send.py
+++ nanosw06/double_send.py
@@ -4,7 +4,7 @@ iface = 'h1-eth0'
 dstMac = b'\xff\xff\xff\xff\xff\xff'
 srcMac = b'\x00\x00\x00\x00\x00\x01'
 proto = b'\x88\xb5' # protocol type is IEEE Local Experimental
-packet = dstMac + srcMac + proto + "012345678901234567890123456789012345678901234567890123456789"
+packet = dstMac + srcMac + proto + b"012345678901234567890123456789012345678901234567890123456789"
 s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3)) # 3:ETH_P_ALL
 s.bind((iface, 0))
 print('send #1 packet.')
